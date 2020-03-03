27f7f02c86426c5d6a79423cf5e5f68334777542
John Lange <lange.john.m@gmail.com>
2020-03-02 21:09:49 -0500

    Add default menu to api

--- ReplayFXSchedule.Web/Controllers/API/APIV2Controller.cs
+++ ReplayFXSchedule.Web/Controllers/API/APIV2Controller.cs
@@ -29,6 +29,16 @@ namespace ReplayFXSchedule.Web.Controllers
             convention.EventTypes = GetEventTypes(convention, showPrivate);
             convention.Vendors = convention.Vendors.OrderBy(e => e.Title).ToList();
             convention.Games = convention.Games.Where(g => g.AtConvention).OrderBy(g => g.GameTitle).ToList();
+            List<Menu> menu = new List<Menu>();
+
+            menu.Add(new Menu { Type = "Schedule", Title = "Schedule" });
+            MenuOption tempOption = new MenuOption { Title = "My Schedule", ScheduleFilter = "my-schedule" };
+            menu.Add(new Menu { Type = "Schedule", Title = "My Schedule", Options = tempOption});
+            menu.Add(new Menu { Type = "EventMenu" });
+            menu.Add(new Menu { Type = "VendorsList", Title = "Vendors" });
+            menu.Add(new Menu { Type = "Sponsors", Title = "Sponsors" });
+            menu.Add(new Menu { Type = "StaticMap", Title = "Map" });
+            convention.Menu = menu;
             return Ok(convention);
         }
 
---
+++ ReplayFXSchedule.Web/Models/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReplayFXSchedule.Web.Models
+{
+    public class Menu
+    {
+        public string Type;
+        public string Title;
+        public MenuOption Options;
+    }
+
+    public class MenuOption
+    {
+        public string Title;
+        public string ScheduleFilter;
+    }
+}
