75af9182a93dcc876ae9dd09ef2268a1f15c7f0c
John Lange <lange.john.m@gmail.com>
2020-07-17 23:48:01 -0400

    Reorder menu for fresh fest

--- ReplayFXSchedule.Web/Controllers/API/APIV2Controller.cs
+++ ReplayFXSchedule.Web/Controllers/API/APIV2Controller.cs
@@ -44,8 +44,8 @@ namespace ReplayFXSchedule.Web.Controllers
                 menu.Add(new Menu { Type = "Schedule", Title = "Schedule" });
                 MenuOption tempOption = new MenuOption { Title = "My Schedule", ScheduleFilter = "my-schedule" };
                 menu.Add(new Menu { Type = "Schedule", Title = "My Schedule", Options = tempOption });
-                menu.Add(new Menu { Type = "EventMenu" });
                 menu.Add(new Menu { Type = "VendorMenu" });
+                menu.Add(new Menu { Type = "EventMenu" });
                 menu.Add(new Menu { Type = "Sponsors", Title = "Sponsors" });
             }
             else
